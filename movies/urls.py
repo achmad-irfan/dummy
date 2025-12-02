902595b6dfd834cecba2e640a4baef4fc824af54
Achmad Irfan <achmad.irfan1@yahoo.com>
2025-12-02 21:36:43 +0700

    hapus hapus 4

--- movies/urls.py
+++ movies/urls.py
@@ -5,10 +5,7 @@ from . import views
 
 app_name='app_movies'
 urlpatterns = [
-    path("",views.index,name='movies'),
-    path("movie/<int:movie_id>/", views.detail_movie, name="detail_movie"),
-    path('search-api/', views.search_api, name='search_api')
-
-
+    path("",views.index,name='movies')
+    
 ]
 
